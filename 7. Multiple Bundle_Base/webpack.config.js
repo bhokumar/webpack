58e29cfa9d329db618b868b8f4080a65dc2f4f6e
bhokumar <bhoopendra.akgec@gmail.com>
2023-08-05 16:20:03 +0530

    Adding changes for webpack html plugin

---
+++ 7. Multiple Bundle_Base/webpack.config.js
@@ -0,0 +1,13 @@
+const path = require("path");
+
+module.exports = {
+    entry: {
+        index: "./src/index.js",
+        product: "./src/products.js",
+    },
+    output: {
+        filename: "[name].bundle.js",
+        path: path.resolve(__dirname, "dist"),
+        clean: true
+    }
+}
